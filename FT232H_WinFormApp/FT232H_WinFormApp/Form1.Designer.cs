4ccfdbeb5ce66a53c7f1d9dc789a3c5b5777e74f
yfusegiAX <y-fusegi@axsl.co.jp>
2022-06-07 10:22:12 +0900

    first commit

---
+++ FT232H_WinFormApp/FT232H_WinFormApp/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿#define FT232H
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using FTD2XX_NET;
+using System.Threading;
+
+namespace FT232H_WinFormApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+
+        //###################################################################################################################################
+        //###################################################################################################################################
+        //##################                                      Definitions                                           #####################
+        //###################################################################################################################################
+        //###################################################################################################################################
+        //driverの定義
+        FTDI.FT_STATUS ftStatus = FTDI.FT_STATUS.FT_OK;//通信可能な状態
+
+        //I2Cのライブラリの定義
+        const byte I2C_Dir_SDAin_SCLin = 0x00;
+        const byte I2C_Dir_SDAin_SCLout = 0x01;
+        const byte I2C_Dir_SDAout_SCLout = 0x03;
+        const byte I2C_Dir_SDAout_SCLin = 0x02;
+        const byte I2C_Data_SDAhi_SCLhi = 0x03;
+        const byte I2C_Data_SDAlo_SCLhi = 0x01;
+        const byte I2C_Data_SDAlo_SCLlo = 0x00;
+        const byte I2C_Data_SDAhi_SCLlo = 0x02;
+
+        // MPSSE clocking commands
+        const byte MSB_FALLING_EDGE_CLOCK_BYTE_IN = 0x24;
+        const byte MSB_RISING_EDGE_CLOCK_BYTE_IN = 0x20;
+        const byte MSB_FALLING_EDGE_CLOCK_BYTE_OUT = 0x11;
+        const byte MSB_DOWN_EDGE_CLOCK_BIT_IN = 0x26;
+        const byte MSB_UP_EDGE_CLOCK_BYTE_IN = 0x20;
+        const byte MSB_UP_EDGE_CLOCK_BYTE_OUT = 0x10;
+        const byte MSB_RISING_EDGE_CLOCK_BIT_IN = 0x22;
+        const byte MSB_FALLING_EDGE_CLOCK_BIT_OUT = 0x13;
+
+        // Clock
+        const uint ClockDivisor = 49;      //          = 199;// for 100KHz
+        // Sending and receiving
+        static uint NumBytesToSend = 0;
+        static uint NumBytesToRead = 0;
+        uint NumBytesSent = 0;
+        static uint NumBytesRead = 0;
+        static byte[] MPSSEbuffer = new byte[500];
+        static byte[] InputBuffer = new byte[500];
+        static byte[] InputBuffer2 = new byte[500];
+        static uint BytesAvailable = 0;
+        static bool I2C_Ack = false;
+        static byte AppStatus = 0;
+        static byte I2C_Status = 0;
+        public bool Running = true;
+        static bool DeviceOpen = false;
+        // GPIO
+        static byte GPIO_Low_Dat = 0;
+        static byte GPIO_Low_Dir = 0;
+        static byte ADbusReadVal = 0;
+        static byte ACbusReadVal = 0;
+
+        // ###### Proximity sensor defines ######
+        static byte Command = 0x00;
+        static byte[] ProxData = new byte[500];
+        static UInt16 ProxiValue = 0;
+        static double ProxiValueD = 0;
+        public const byte VCNL40x0_ADDRESS = 0x13;//0x13 is 7 bit address, 0x26 is 8bit address
+        // registers
+        public const byte REGISTER_COMMAND = 0x80;
+        public const byte REGISTER_ID = 0x81;
+        public const byte REGISTER_PROX_RATE = 0x82;
+        public const byte REGISTER_PROX_CURRENT = 0x83;
+        public const byte REGISTER_AMBI_PARAMETER = 0x84;
+        public const byte REGISTER_AMBI_VALUE = 0x85;
+        public const byte REGISTER_PROX_VALUE = 0x87;
+        public const byte REGISTER_INTERRUPT_CONTROL = 0x89;
+        public const byte REGISTER_INTERRUPT_LOW_THRES = 0x8a;
+        public const byte REGISTER_INTERRUPT_HIGH_THRES = 0x8c;
+        public const byte REGISTER_INTERRUPT_STATUS = 0x8e;
+        public const byte REGISTER_PROX_TIMING = 0xf9;
+        // Bits in the registers defined above
+        public const byte COMMAND_SELFTIMED_MODE_ENABLE = 0x01;
+        public const byte COMMAND_PROX_ENABLE = 0x02;
+        public const byte COMMAND_AMBI_ENABLE = 0x04;
+        public const byte COMMAND_MASK_PROX_DATA_READY = 0x20;
+        public const byte PROX_MEASUREMENT_RATE_31 = 0x04;
+        public const byte AMBI_PARA_AVERAGE_32 = 0x05; // DEFAULT
+        public const byte AMBI_PARA_AUTO_OFFSET_ENABLE = 0x08; // DEFAULT enable
+        public const byte AMBI_PARA_MEAS_RATE_2 = 0x10; // DEFAULT
+        public const byte INTERRUPT_THRES_SEL_PROX = 0x00;
+        public const byte INTERRUPT_THRES_ENABLE = 0x02;
+        public const byte INTERRUPT_COUNT_EXCEED_1 = 0x00; // DEFAULT
+
+        // ###### Colour sensor defines ######
+        public const byte COLOR_ADDRESS = 0x29;
+        public const byte _ENABLE = 0x80;                   //Enablestatusandinterrupts
+        public const byte _ATIME = 0x81;                    //RGBCADCtime
+        public const byte _CONTROL = 0x8F;                  //Gaincontrolregister
+        public const byte _GAIN_x4 = 0x01;
+        public const byte _GAIN_x16 = 0x10;
+        public const byte _GAIN_x60 = 0x11;
+        static byte Global_Red = 0;
+        static byte Global_Green = 0;
+        static byte Global_Blue = 0;
+        uint devcount = 0;
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(226, 377);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(398, 377);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(109, 42);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(73, 93);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(378, 179);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Display";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Location = new System.Drawing.Point(503, 102);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(245, 67);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Interface";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Location = new System.Drawing.Point(501, 193);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(248, 131);
+            this.groupBox3.TabIndex = 5;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Sensors";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private GroupBox groupBox1;
+        private GroupBox groupBox2;
+        private GroupBox groupBox3;
+    }
+}
---
+++ FT232H_WinFormApp/FT232H_WinFormApp/Form1.cs
@@ -0,0 +1,20 @@
+namespace FT232H_WinFormApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
